1a497333ace419b29e1dbc0cbd19991755b6a3d7
rsca7213 <rsca4321@gmail.com>
2020-09-21 19:40:15 -0400

    added return to start button for secondaries

--- js/obras/obras.js
+++ js/obras/obras.js
@@ -1,8 +1,18 @@
 const contactNav = document.getElementById('contact-nav');
+const inicioBtn = document.getElementById('inicio-btn');
+
 contactNav.addEventListener("click", () => {
     window.scrollTo({
         top: document.body.scrollHeight,
         left: 0,
         behavior: 'smooth'
     });
+});
+
+inicioBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    })
 });
